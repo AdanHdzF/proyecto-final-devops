ba133184a7927e15f34e5e6e960f69e3697e7c34
AdánHF <35301303+AdanHdzF@users.noreply.github.com>
2025-07-30 10:37:23 -0600

    Add javalin basic server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static Javalin createApp() {
+		// Javalin app = Javalin.create(config -> {
+		// 	config.enableCorsForAllOrigins();
+		// 	config.defaultContentType = "application/json";
+		// });
+		
+		// app.get("/", ctx -> ctx.result("Hello World!"));
+		
+		// return app;
+		return Javalin.create().get("/hello", ctx -> ctx.result("Hello World!"));
+	}
+
+	public static void main(String[] args) {
+		Javalin app = createApp();
+		app.start(7000);
+		System.out.println("Server started on port 7000");
+	}
 }
